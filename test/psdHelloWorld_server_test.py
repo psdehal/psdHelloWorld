2c0b0ffe682f45efe36b8e2a476026f114f843c4
Paramvir Dehal <psdehal@lbl.gov>
2018-10-22 22:33:46 +0000

    add impl

--- test/psdHelloWorld_server_test.py
+++ test/psdHelloWorld_server_test.py
@@ -76,14 +76,8 @@ class psdHelloWorldTest(unittest.TestCase):
         return self.__class__.ctx
 
     # NOTE: According to Python unittest naming rules test method names should start from 'test'. # noqa
-    def test_your_method(self):
-        # Prepare test objects in workspace if needed using
-        # self.getWsClient().save_objects({'workspace': self.getWsName(),
-        #                                  'objects': []})
-        #
-        # Run your method by
-        # ret = self.getImpl().your_method(self.getContext(), parameters...)
-        #
-        # Check returned data with
-        # self.assertEqual(ret[...], ...) or other unittest methods
-        pass
+    def test_printhelloworld(self):
+        result = self.getImpl().printhelloworld(self.getContext(), {
+                'phrase': "HelloWorld"
+            })[0]
+        print "TEST file and your phrase is " + result['phrase'] + "\n"
